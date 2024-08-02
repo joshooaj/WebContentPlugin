6fe5295f3f6706f09b204dc02213bf97c34dad51
Josh Hendricks <joshooaj@gmail.com>
2024-08-02 08:53:02 -0700

    chore: move webview2 installer download location to the Setup project folder

--- build.ps1
+++ build.ps1
@@ -9,12 +9,13 @@ param(
 & dotnet tool restore
 
 $null = New-Item $PSScriptRoot\output -ItemType Directory -Force
+Get-ChildItem $PSScriptRoot\output | Remove-Item -Recurse -Force
 
 # Download WebView2 runtime
 $webview2Url = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/c638b5d5-4b2f-4845-baab-c9f4fd6c58ab/MicrosoftEdgeWebView2RuntimeInstallerX64.exe'
-if (!(Test-Path $PSScriptRoot\output\MicrosoftEdgeWebView2RuntimeInstallerX64.exe)) {
+if (!(Test-Path $PSScriptRoot\src\Setup\MicrosoftEdgeWebView2RuntimeInstallerX64.exe)) {
     $ProgressPreference = 'SilentlyContinue'
-    Invoke-WebRequest -Uri $webview2Url -OutFile $PSScriptRoot\output\MicrosoftEdgeWebView2RuntimeInstallerX64.exe
+    Invoke-WebRequest -Uri $webview2Url -OutFile $PSScriptRoot\src\Setup\MicrosoftEdgeWebView2RuntimeInstallerX64.exe
 }
 
 if (!$PSCmdlet.MyInvocation.BoundParameters.ContainsKey('Version')) {
@@ -22,3 +23,9 @@ if (!$PSCmdlet.MyInvocation.BoundParameters.ContainsKey('Version')) {
 }
 
 & msbuild src/Setup/setup.wixproj /t:Rebuild /restore:True /p:Configuration=Release /p:Platform=x64 /p:BuildVersion=$Version
+
+if (!$?) {
+    throw "MSBuild exited with code $LASTEXITCODE"
+}
+
+Get-ChildItem $PSScriptRoot\output\Setup\ | Get-FileHash | Select-Object @{n='File';e={([io.fileinfo]$_.Path).Name}}, Hash, Algorithm | ConvertTo-Json | Set-Content $PSScriptRoot\output\Setup\sha256.json
--- install.ps1
+++ install.ps1
@@ -2,4 +2,5 @@ Get-Date
 & appwiz.cpl
 ii C:\repos\WebContentPlugin
 ii 'C:\Program Files\'
-msiexec /i c:\repos\WebContentPlugin\output\Installer\setup.msi /q /le c:\repos\WebContentPlugin\output\Installer\install.log
+$msiPath = (Get-ChildItem -Path C:\repos\WebContentPlugin\output\Setup\*.msi).FullName
+msiexec /i $msiPath /q /le c:\repos\WebContentPlugin\output\Setup\install.log
