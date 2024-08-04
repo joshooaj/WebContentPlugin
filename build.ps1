e393c68dd554d1eebcb28a6867912a400290f300
Josh Hendricks <joshooaj@gmail.com>
2024-08-03 18:02:20 -0700

    ci: add initial github action workflow

--- build.ps1
+++ build.ps1
@@ -13,19 +13,28 @@ Get-ChildItem $PSScriptRoot\output | Remove-Item -Recurse -Force
 
 # Download WebView2 runtime
 $webview2Url = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/c638b5d5-4b2f-4845-baab-c9f4fd6c58ab/MicrosoftEdgeWebView2RuntimeInstallerX64.exe'
-if (!(Test-Path $PSScriptRoot\src\Setup\MicrosoftEdgeWebView2RuntimeInstallerX64.exe)) {
+$webviewDestination = [io.path]::Combine($PSScriptRoot, '.cache', 'MicrosoftEdgeWebView2RuntimeInstallerX64.exe')
+if (!(Test-Path $webviewDestination -PathType Leaf)) {
     $ProgressPreference = 'SilentlyContinue'
-    Invoke-WebRequest -Uri $webview2Url -OutFile $PSScriptRoot\src\Setup\MicrosoftEdgeWebView2RuntimeInstallerX64.exe
+    Write-Host "Downloading WebView2 Runtime due to cache miss" -ForegroundColor Yellow
+    Invoke-WebRequest -Uri $webview2Url -OutFile $webviewDestination
 }
 
 if (!$PSCmdlet.MyInvocation.BoundParameters.ContainsKey('Version')) {
-    #$Version = (dotnet nbgv get-version -f json | ConvertFrom-Json).SimpleVersion
     $Version = (dotnet nbgv get-version -f json | ConvertFrom-Json).Version
 }
+
+Write-Host "Building CascadiaWebContentPlugin.$Version.msi"
+
 & msbuild src/Setup/setup.wixproj /t:Rebuild /restore:True /p:Configuration=Release /p:Platform=x64 /p:BuildVersion=$Version
 
 if (!$?) {
     throw "MSBuild exited with code $LASTEXITCODE"
 }
+Write-Host "Build completed successfully." -ForegroundColor Green
 
-Get-ChildItem $PSScriptRoot\output\Setup\ | Get-FileHash | Select-Object @{n='File';e={([io.fileinfo]$_.Path).Name}}, Hash, Algorithm | ConvertTo-Json | Set-Content $PSScriptRoot\output\Setup\sha256.json
+[pscustomobject]@{
+    Version   = $Version
+    Artifacts = Resolve-Path $PSScriptRoot\output\WebContent
+    Installer = Resolve-Path $PSScriptRoot\output\Setup\en-US\
+}
